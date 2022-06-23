b9ff09b51d13c7092040f1dc8de546b807792510
subhashana77 <106365502+subhashana77@users.noreply.github.com>
2022-06-23 16:58:21 +0530

    Rest API data fetching url part done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Util.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from "react";
+
+const API_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ed5bdb9a00ac4151b1b103c92bbbd65b";
+
+const Util = () => {
+    const [news, setNews] = useState([]);
+
+    const searchNews = async () => {
+        const response = await fetch(`${API_URL}`);
+        const data = await  response.json();
+        setNews(data.articles);
+        console.log(data.articles);
+    }
+
+    useEffect(() => {
+        searchNews();
+    });
+    return (
+        <>
+        </>
+    );
+}
+
+export default Util;
